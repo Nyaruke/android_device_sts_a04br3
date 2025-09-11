a45c96f3dd60368fc98f31c5598c20a20015a174
Nyaruke <119842878+Nyaruke@users.noreply.github.com>
2025-09-11 16:02:18 +0900

    saved

--- device.mk
+++ device.mk
@@ -18,17 +18,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.mt8167.usb.rc:root/init.mt8167.usb.rc \
     $(LOCAL_PATH)/rootdir/etc/fstab.mt8167:root/fstab.mt8167 \
     $(LOCAL_PATH)/rootdir/etc/init.recovery.mt8167.rc:root/init.recovery.mt8167.rc \
-    $(LOCAL_PATH)/rootdir/etc/ueventd.mt8167.rc:root/ueventd.mt8167.rc
-#    $(LOCAL_PATH)/rootdir/etc/init.common_svc.rc:root/init.common_svc.rc \
-#    $(LOCAL_PATH)/rootdir/etc/meta_init.rc:root/meta_init.rc \
-#    $(LOCAL_PATH)/rootdir/etc/meta_init.project.rc:root/meta_init.project.rc \
-#    $(LOCAL_PATH)/rootdir/etc/meta_init.connectivity.rc:root/meta_init.connectivity.rc \
-#    $(LOCAL_PATH)/rootdir/etc/meta_init.modem.rc:root/meta_init.modem.rc \
-#    $(LOCAL_PATH)/rootdir/etc/init.connectivity.rc:root/init.connectivity.rc \
-#    $(LOCAL_PATH)/rootdir/etc/init.project.rc:root/init.project.rc \
-#    $(LOCAL_PATH)/rootdir/etc/init.aee.rc:root/init.aee.rc \
-#    $(LOCAL_PATH)/rootdir/etc/init.rilproxy.rc:root/init.rilproxy.rc \
-#    $(LOCAL_PATH)/rootdir/etc/init.modem.rc:root/init.modem.rc
+    $(LOCAL_PATH)/rootdir/etc/ueventd.mt8167.rc:root/ueventd.mt8167.rc \
+    $(LOCAL_PATH)/rootdir/etc/init.connectivity.rc:root/init.connectivity.rc \
+    $(LOCAL_PATH)/rootdir/etc/init.project.rc:root/init.project.rc \
+    $(LOCAL_PATH)/rootdir/etc/init.aee.rc:root/init.aee.rc \
+    $(LOCAL_PATH)/rootdir/etc/init.rilproxy.rc:root/init.rilproxy.rc \
+    $(LOCAL_PATH)/rootdir/etc/init.modem.rc:root/init.modem.rc
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/recovery/multi_init.rc:recovery/root/multi_init.rc \
