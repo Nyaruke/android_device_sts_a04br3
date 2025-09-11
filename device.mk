8c6c248d2d0dc075a30e00eeebb0466c52898653
Nyaruke <119842878+Nyaruke@users.noreply.github.com>
2025-09-11 14:10:47 +0900

    saved

--- device.mk
+++ device.mk
@@ -24,13 +24,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.aee.rc:root/init.aee.rc \
     $(LOCAL_PATH)/rootdir/etc/init.rilproxy.rc:root/init.rilproxy.rc \
     $(LOCAL_PATH)/rootdir/etc/init.modem.rc:root/init.modem.rc \
-    $(LOCAL_PATH)/rootdir/etc/enableswap.sh:root/enableswap.sh \
     $(LOCAL_PATH)/rootdir/etc/init.mt8167.usb.rc:root/init.mt8167.usb.rc \
     $(LOCAL_PATH)/rootdir/etc/init.common_svc.rc:root/init.common_svc.rc \
     $(LOCAL_PATH)/rootdir/etc/fstab.mt8167:root/fstab.mt8167 \
     $(LOCAL_PATH)/rootdir/etc/init.recovery.mt8167.rc:root/init.recovery.mt8167.rc \
+    $(LOCAL_PATH)/rootdir/etc/ueventd.rc:root/ueventd.rc \
     $(LOCAL_PATH)/rootdir/etc/ueventd.mt8167.rc:root/ueventd.mt8167.rc
-        
+    #$(LOCAL_PATH)/rootdir/etc/enableswap.sh:root/enableswap.sh \ 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/recovery/multi_init.rc:recovery/root/multi_init.rc \
     $(LOCAL_PATH)/rootdir/recovery/factory_init.connectivity.rc:recovery/root/factory_init.connectivity.rc \
