a58de8a8d7f90ab9ced7d19d7e690b5612656d87
Nyaruke <119842878+Nyaruke@users.noreply.github.com>
2025-09-16 21:12:50 +0900

    saved

--- build-utils/auto_buildenv_setup.sh
+++ build-utils/auto_buildenv_setup.sh
@@ -14,11 +14,3 @@ git config --global user.email "android"
 # setup lineage source
 yes n | sh -c "repo init -u https://github.com/LineageOS/android.git -b cm-14.1 --depth=1"
 repo sync -j8 -f --force-sync --no-clone-bundle
-
-# setup vendor
-git clone https://github.com/mouseos/vendor_benesse_TAB-A04-BR3.git vendor/sts/a04br3
-mv vendor/sts/a04br3/TAB-A04-BR3-vendor.mk vendor/sts/a04br3/a04br3-vendor.mk
-sed -i 's/TAB-A04-BR3/a04br3/g' vendor/sts/a04br3/a04br3-vendor.mk
-sed -i 's/benesse/sts/g' vendor/sts/a04br3/a04br3-vendor.mk
-sed -i 's/TAB-A04-BR3/a04br3/g' vendor/sts/a04br3/Android.mk
-sed -i 's/benesse/sts/g' vendor/sts/a04br3/Android.mk
