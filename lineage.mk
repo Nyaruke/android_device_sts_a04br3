cae4f53d908405cf61edb09d36247f4e080e479a
Nyaruke <119842878+Nyaruke@users.noreply.github.com>
2025-09-18 22:11:35 +0900

    saved

--- lineage.mk
+++
@@ -1,5 +0,0 @@
-# Inherit device configuration
-$(call inherit-product, device/sts/a04br3/lineage_a04br3.mk)
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
-
-TARGET_LOCALES := ja_JP en_US
